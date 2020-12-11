a86cb0b4af29d80a0a0acb862d803b5c4f1bba2a
Benjamin Goff <bgoff@mainerobotics.org>
2020-12-11 09:57:34 -0500

    Add project files.

---
+++ coding12-11/FindDays.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace coding12_11
+{
+    class FindDays
+    {
+        //Enemeration setup
+        enum DaysOfTheWeek
+        {
+            SUNDAY = 1, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+        }
+        static void Main(string[] args)
+        {
+            DaysOfTheWeek day; //create an instance of our Enumerator 
+            int userInput;
+
+            Console.WriteLine("Enter a number to the coresponding day of the week: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            day = (DaysOfTheWeek)userInput; //casting the user input to corresponding Day of the Week
+
+            Console.WriteLine("{0}th day of the week is {1}", userInput, day);
+        }
+    }
+}
